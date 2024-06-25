6839e58450858cc550b7065bee5a1c41ba5f99ed
AbdullahA12345 <160193613+AbdullahA12345@users.noreply.github.com>
2024-06-25 13:44:59 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,14 @@
+// Create function addUser()
+function addUser() { 
+  // Get value of user by id player1_name_input and player2_name_input
+ document.getElementById("addUser").innerHTML
+
+  // Store these values locally
+score=localStorage.getItem("addUser");
+  //Assign "game_page.html" to window.location
+window.location="game_page.html";
+}
+
+
+
+
